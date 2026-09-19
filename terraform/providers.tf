92c36bdeafa3491eb4806f6b095e7affdd54a6a5
Melvyn de Kort <melvyn@mdekort.nl>
2026-09-19 13:06:00 +0200

    Remove test comment from trigger-deploy validation

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -21,4 +21,3 @@ provider "grafana" {
   url  = data.terraform_remote_state.tf_grafana.outputs.grafana_url
   auth = data.terraform_remote_state.tf_grafana.outputs.kids_monitor_token
 }
-# test: validating auto-deploy-on-merge automation
